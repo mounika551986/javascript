ed04497f8e10e5401f62f9cc0ea8dd7e3ab0b36b
mounika551986 <mounika.katirevula@nybinfotech.com>
2026-05-25 18:03:35 +0530

    Add files via upload

---
+++ 25_05_2026vi7.js
@@ -0,0 +1,40 @@
+console.log("this is js file")
+console.log("hello")
+console.log("hai")
+
+
+let applePrice = 10;
+let bananaPrice = 20;
+let totalCost = applePrice + bananaPrice;
+
+console.log("Total price is: " + totalCost); 
+
+const userName = "Alice"; // 'const' because the name won't change here
+let greeting = `Hello, ${userName}! Welcome to the site.`;
+
+console.log(greeting); 
+
+// creating 3. Updating Variable Values
+
+let score = 0;
+console.log(score); // 0
+
+// Increasing the value
+score += 10; 
+console.log(score); // 10
+
+// Reassigning a completely new value
+score = 25;
+console.log(score); // 25
+
+var a=10;
+var b=20;
+console.log(a+b);
+
+
+
+
+
+
+
+
